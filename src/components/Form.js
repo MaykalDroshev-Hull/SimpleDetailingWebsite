753fb13713ae25def3a1a1476b0c01474bc69465
Lofton <cary.l.gentry.jr@gmail.com>
2023-03-30 16:01:57 -0400

    Fixed navbar and updated form styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Input, Spacer } from '@nextui-org/react'
 import styles from '../styles/Form.module.css'
 import buttonStyles from '../styles/FlowButton.module.css'
 
@@ -31,36 +32,24 @@ const Form = (props) => {
     <div className={styles.modal}>
       <div className={styles.modalContent}>
         <span className="close" onClick={props.onClick}>&times;</span>
-        <h1>Form</h1>
+        <h1>Submit Request</h1>
         <form className={styles.form} onSubmit={handleSubmit}>
-          <label>
-            Label
-            <input type='text' name='firstName' value={formState.firstName} onChange={handleInputChange} placeholder='First Name' required />
-          </label>
-          <label>
-            Label
-            <input type='text' name='lastName' value={formState.lastName} onChange={handleInputChange} placeholder='Last Name' required />
-          </label>
-          <label>
-            Label
-            <input type='email' name='email' value={formState.email} onChange={handleInputChange} placeholder='Email' required />
-          </label>
-          <label>
-            Label
-            <input type='tel' name='phoneNumber' value={formState.phoneNumber} onChange={handleInputChange} placeholder='Phone Number' />
-          </label>
-          <label>
-            Label
-            <input type='text' name='carMake' value={formState.carMake} onChange={handleInputChange} placeholder='Car Make' />
-          </label>
-          <label>
-            Label
-            <input type='text' name='carModel' value={formState.carModel} onChange={handleInputChange} placeholder='Car Model' />
-          </label>
-          <label>
-            Desired Date:
-            <input type='date' name='desiredDate' value={formState.desiredDate} onChange={handleInputChange} />
-          </label>
+          <Spacer y={1.6} />
+          <Input type='text' name='firstName' value={formState.firstName} onChange={handleInputChange} labelPlaceholder='First Name' required clearable width='300px'/>
+          <Spacer y={1.6} />
+          <Input type='text' name='lastName' value={formState.lastName} onChange={handleInputChange} labelPlaceholder='Last Name' required clearable width='300px'/>
+          <Spacer y={1.6} />
+          <Input type='email' name='email' value={formState.email} onChange={handleInputChange} labelPlaceholder='Email' required clearable width='300px'/>
+          <Spacer y={1.6} />
+          <Input type='tel' name='phoneNumber' value={formState.phoneNumber} onChange={handleInputChange} labelPlaceholder='Phone Number' clearable width='300px'/>
+          <Spacer y={1.6} />
+          <Input type='text' name='carMake' value={formState.carMake} onChange={handleInputChange} labelPlaceholder='Car Make' clearable width='300px'/>
+          <Spacer y={1.6} />
+          <Input type='text' name='carModel' value={formState.carModel} onChange={handleInputChange} labelPlaceholder='Car Model' clearable width='300px'/>
+          <Spacer y={.75} />
+          <h4>Desired Date</h4>
+          <Input type='date' name='desiredDate' value={formState.desiredDate} onChange={handleInputChange} width='300px'/>
+          <Spacer y={1.6} />
           <button className={buttonStyles.button} type='submit'>Submit</button>
         </form>
       </div>
