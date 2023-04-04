f613e9a847b1e5b5620b227d23ff8a6c4c793aa2
Lofton <cary.l.gentry.jr@gmail.com>
2023-04-04 17:32:12 -0400

    Variable error update

--- src/pages/api/review.js
+++ src/pages/api/review.js
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client"
 const handler = async (req, res) => {
   if (req.method === 'POST') {
     const data = req.body
-    if (!data.firstName || !data.lastName || !data.review) {
+    if (!data.firstName || !data.lastName || !data.reviewText) {
       return res.status(400).json({ message: 'Bad Request' })
     } else {
       return await addReview(req, res)
