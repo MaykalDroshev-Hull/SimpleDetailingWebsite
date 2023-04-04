8ecb8c885101693d3643c567986a5dadc8424cd1
Lofton <cary.l.gentry.jr@gmail.com>
2023-04-04 17:32:30 -0400

    Cards to display data pulled from database

--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -1,9 +1,13 @@
-import { Button, useDisclosure } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { useMediaQuery, useBreakpointValue, Card, CardBody, Flex, Heading, SimpleGrid, useDisclosure, Text } from '@chakra-ui/react'
+import { PrismaClient } from '@prisma/client'
 import PageTitle from '@/components/PageTitle'
-import buttonStyles from '../styles/FlowButton.module.css'
 import ReviewForm from '@/components/ReviewForm'
+import buttonStyles from '../styles/FlowButton.module.css'
 
-const reviews = () => {
+const reviews = ({ data }) => {
+  const [entries, setEntries] = useState(data)
+  const cardWidth = useBreakpointValue({ base: '100%', md: 'minmax(700px, 1fr)' })
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
@@ -13,8 +17,29 @@ const reviews = () => {
         <button onClick={onOpen} className={buttonStyles.button}> Submit a Review!</button>
         <ReviewForm isOpen={isOpen} onClose={onClose} />
       </div>
+      <Flex justifyContent='center'>
+          <SimpleGrid spacing={4} gridTemplateColumns={`repeat(auto-fit, ${cardWidth})`}>
+            {data.map((review) => (
+              <Card key={review.id} padding={4} margin={4}>
+                <Heading size='lg'>{review.firstName} {review.lastName}</Heading>
+                <CardBody>
+                  <Text>
+                    {review.reviewText}
+                  </Text>
+                </CardBody>
+              </Card>
+            ))}
+          </SimpleGrid>
+      </Flex>
     </>
   )
+}
+
+export async function getServerSideProps() {
+  const prisma = new PrismaClient()
+  const data = await prisma.review.findMany()
+
+  return { props: { data } }
 }
 
 export default reviews
