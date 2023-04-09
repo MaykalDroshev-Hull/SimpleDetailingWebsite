d23b47ab7d12b6f5dfa43d5e216bb6aacbb20163
Lofton <cary.l.gentry.jr@gmail.com>
2023-04-08 23:49:43 -0400

    new longContactForm API

--- src/lib/api.js
+++ src/lib/api.js
@@ -7,11 +7,24 @@ export const sendContactForm = async (data) =>
       Accept: 'application/JSON'
     },
   }).then(res => {
-    if(!res.ok) throw new Error('Failed to send message')
+    if (!res.ok) throw new Error('Failed to send message')
     return res.json()
   })
 
-export const sendReviewForm = async (data) => 
+export const sendLongContactForm = async (data) =>
+  fetch('/api/longContact', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/JSON'
+    },
+  }).then(res => {
+    if (!res.ok) throw new Error('Failed to send message')
+    return res.json()
+  })
+
+export const sendReviewForm = async (data) =>
   fetch('/api/review', {
     method: 'POST',
     body: JSON.stringify(data),
@@ -19,6 +32,7 @@ export const sendReviewForm = async (data) =>
       'Content-Type': 'application/json'
     }
   }).then(res => {
-    if(!res.ok) throw new Error('Failed to submit review')
+    if (!res.ok) throw new Error('Failed to submit review')
     return res.json()
   })
+
